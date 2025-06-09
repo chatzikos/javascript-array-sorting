b541e8aee47dcb89e0e0a106d28bd098fae3f8e2
Nick Chatzikos <=>
2025-06-09 11:09:14 +0300

    first commit

---
+++ products.js
@@ -0,0 +1,32 @@
+const products = [
+    {
+        title: 'Wireless Bluetooth Headphones',
+        price: 49.99,
+        image: './img/wireless-headphones.jpg',
+        category: 'electronics',
+    },
+    {
+        title: 'Smart LED Light Bulb',
+        price: 0,
+        image: './img/light-bulb.png',
+        category: 'appliances',
+    },        
+    {
+        title: 'Mobile Phone',
+        price: 1100,
+        image: './img/mobile-phone.webp',
+        category: 'electronics',
+    },
+    {
+        title: 'Microwavable Pizza',
+        price: 7,
+        image: './img/microwavable-pizza.png',
+        category: 'food-items',
+    },
+    {
+        title: 'Bottle of Water',
+        price: 0.5,
+        image: './img/water-bottle.png',
+        category: 'food-items',
+    },               
+];
